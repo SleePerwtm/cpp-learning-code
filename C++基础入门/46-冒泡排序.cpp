5f1dbed25446d19da7bfbc097891bfc6fde2192e
SleePerwtm <wtm2785419816@outlook.com>
2025-09-14 15:59:32 +0800

    feat: 添加用户输入和排序输出功能

--- C++基础入门/46-冒泡排序.cpp
+++ C++基础入门/46-冒泡排序.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 void bubbleSort(int *a, int size);
 
 int main()
 {
-    int a[10002];
-    int size = sizeof(a) / sizeof(a[0]);
+    int size, a[10002];
+    memset(a, 0, sizeof(a));
+    cout << "输入数组长度：";
+    cin >> size;
+    cout << "输入数组元素，元素间用空格分割：";
+    for (int i = 0; i < size; i++)
+        cin >> a[i];
     bubbleSort(a, size);
+    cout << endl
+         << "排序后的数组为：";
     for (int i = 0; i < size; i++)
         cout << a[i] << " ";
 }

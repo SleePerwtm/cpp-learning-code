e245809817252cf9d55680e567176557d4917664
SleePerwtm <wtm2785419816@outlook.com>
2025-09-18 19:48:27 +0800

    feat: 添加计算1到10整数和的do-while循环实现

---
+++ 面向对象程序设计实验课/25.09.18/task2-do-while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int sum = 0, i = 1;
+    do
+    {
+        sum += i;
+        i++;
+    } while (i <= 10);
+    cout << sum << endl;
+    return 0;
+}

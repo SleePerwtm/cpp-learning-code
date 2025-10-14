6f055530d22a3274d28c046c5f218b58e2f2aac1
SleePerwtm <wtm2785419816@outlook.com>
2025-10-14 21:44:04 +0800

    feat: 添加CPU类实现
    在代码中添加了一个新的CPU类，该类用于表示CPU的相关属性和操作。
    
    - 定义了一个枚举类型CPU_Rank，用于表示CPU的不同等级。
    - 实现了一个CPU类，其中包含成员变量rank（CPU等级）、frequency（CPU频率）和voltage（CPU电压）。
    - 提供了构造函数和析构函数，分别在创建和销毁CPU对象时输出相应的信息。
    - 实现了获取和设置上述成员变量的方法，例如getRank()、setRank()、getFreq()、setFreq()、getVoltage()和setVoltage()。
    - 在main函数中创建了一个CPU对象，并调用了run()和stop()方法来模拟CPU的运行和停止。

---
+++ 面向对象程序设计实验课/25.10.14/task1-CPU-class.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+enum CPU_Rank { P1 = 1, P2, P3, P4, P5, P6, P7 };
+
+class CPU {
+  public:
+    void run() { std::cout << "CPU开始运行" << std::endl; }
+    void stop() { std::cout << "CPU停止运行" << std::endl; }
+    CPU(enum CPU_Rank r, int f, double v) : rank(r), frequency(f), voltage(v) {
+        std::cout << "CPU的构造函数调用" << std::endl;
+    }
+    ~CPU() { std::cout << "CPU的析构函数调用" << std::endl; }
+
+    enum CPU_Rank getRank() { return rank; }
+    int getFreq() { return frequency; }
+    double getVoltage() { return voltage; }
+
+    void setRank(enum CPU_Rank r) { rank = r; }
+    void setFreq(int f) { frequency = f; }
+    void setVoltage(double v) { voltage = v; }
+
+  private:
+    enum CPU_Rank rank;
+    int frequency;
+    double voltage;
+};
+
+int main() {
+    CPU cpu(P2, 200, 1.0);
+    cpu.run();
+    cpu.stop();
+}
